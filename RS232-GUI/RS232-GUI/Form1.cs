efc96c0f524844a6cbb92ae63311af39534d6c88
Wojciech Będkowski <woj.bedkowski@gmail.com>
2017-09-27 22:48:13 +0200

    Added transactions.

--- RS232-GUI/RS232-GUI/Form1.cs
+++ RS232-GUI/RS232-GUI/Form1.cs
@@ -20,13 +20,47 @@ namespace RS232_GUI
         byte[] pongArr = new byte[] { 80, 79, 78, 71 };
         byte[] pingArr = new byte[] { 80, 73, 78, 71 };
         private Timer timer = new Timer();
+        List<byte> buffer = new List<byte>();
+        bool isTransaction = false;
+        bool pingWait = false;
+        int retransmisions = 0;
+        byte[] lastSentMsg;
 
         public Form1()
         {
             InitializeComponent();
             DisableConnectFunction();
+            timer.Tick += this.Timer_Tick;
 
+        }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (isTransaction)
+            {
+                timer.Stop();
+                buffer.Clear();
+                if (retransmisions > 0)
+                {
+                    retransmisions--;
+                    //log retransmit;
+                    StartTransaction(lastSentMsg);
+                }
+                else
+                {
+                    //log transaction failed;
+                    if (pingWait)
+                    {
+                        LogMessage("Ping timeout");
+                    } else
+                    {
+                        LogMessage("Transakcja zakończona niepowodzeniem");
+                    }
+                    pingWait = false;
+                    isTransaction = false;
+                    retransmisions = 0;
+                }
+            }
         }
 
         public void DisableConnectFunction()
@@ -101,36 +135,137 @@ namespace RS232_GUI
             }
         }
 
-        private void sendMsg(byte[] msg)
-        {
-            try
-            {
-                serialPort.Write(msg, 0, msg.Length);
-            }
-            catch (Exception ex)
-            {
-                LogMessage(ex.Message);
-                return;
-            }
-        }
-
         public void LogMessage(string message)
         {
             System.Windows.Forms.MessageBox.Show(message);
         }
 
-
+        private void OnDataReceived(object sender, EventArgs e)
+        {
+            int bytes = serialPort.BytesToRead;
+            byte[] buff = new byte[bytes];
+            LogIpunt.AppendText("Odebrano wiadomość:/n" + StringParser.ByteToDisplay(buff, DataFormat.HEX) + " -> " + StringParser.ByteToDisplay(buff));
+            serialPort.Read(buff, 0, bytes);
+            buffer.AddRange(buff);
+            if (pingWait)
+            {
+                if (buffer.Count < 4)
+                {
+                    return;
+                }
+                else
+                {
+                    if (IsPong(buffer.ToArray()))
+                    {
+                        LogMessage("Ping sukces");
+                        EndTransaction();
+                        return;
+                    }
+                }
+            }
+            if (IsPing(buff))
+            {
+                this.sendMsg(pongArr);
+                buffer.Clear();
+                return;
+            }
+            if (TabControl.SelectedTab.Text == "RS232")
+            {
+                buffer.Clear();
+                return;
+            }
+            //tutaj modbus
+            int beginIndex = buffer.FindIndex((byte x) => { return x == 58; });
+            if(beginIndex == -1)
+            {
+                buffer.Clear();
+                return;
+            }
+            for(int i = 0; i < beginIndex; i++)
+            {
+                buffer.RemoveAt(0);
+            }
+            if(buffer.Count >= 3)
+            {
+                if((buffer[buffer.Count -1] == 10)&&(buffer[buffer.Count-2] == 13))
+                {
+                    if(!StringParser.CheckModbusMessage(buffer.ToArray(), (byte)numericUpDown7.Value))
+                    {
+                        buffer.Clear();
+                        LogIpunt.AppendText("Niepoprawna ramka/n");
+                        return;
+                    }
+                } else
+                {
+                    return;
+                }
+            } else
+            {
+                return;
+            }
+            if (MasterButtonRadio.Checked)
+            {
+                if (!isTransaction)
+                {
+                    buffer.Clear();
+                    return;
+                }
+                LogMessage("Transakcja zakończona powodzeniem");
+                if(numericUpDown6.Value == 2)
+                {
+                    ReceiveTextPanelASCII.AppendText(StringParser.ByteToDisplay(StringParser.GetModbusMessage(buffer.ToArray())));
+                } else
+                {
+                    ReceiveTextPanelASCII.AppendText("/n");
+                }
+                ReceiveTextPanelFrame.AppendText(StringParser.ByteToDisplay(buffer.ToArray()));
+                buffer.Clear();
+                EndTransaction();
+                return;
+            }
+            if(buffer[4] == 50)
+            {
+                string msgString = StringParser
+                .FormatMessageToModbus(textBox1.Text, 2, (byte)numericUpDown7.Value);
+                byte[] msg = StringParser.StrToByteArray(msgString);
+                sendMsg(msg);
+            }
+            if((buffer[1] == 48)&&((buffer[2] == 48)))
+            {
+                string msgString = StringParser
+                .FormatMessageToModbus("", 1, (byte)numericUpDown7.Value);
+                byte[] msg = StringParser.StrToByteArray(msgString);
+                sendMsg(msg);
+            }
+            buffer.Clear();
+            //wypisz co dostales
+        }
 
         #region Śmieci
 
 
-        private void OnDataReceived(object sender, EventArgs e)
+
+
+
+        private void SettingListBitDataPool_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
 
+        private void SettingListStop_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
+        }
 
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void SettingControlFlewComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
 
 
 
@@ -179,17 +314,9 @@ namespace RS232_GUI
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
+        
 
         private void RSTerminatorLabel2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click_2(object sender, EventArgs e)
         {
 
         }
@@ -310,24 +437,116 @@ namespace RS232_GUI
             SettingSpeedComboBox.SelectedIndex = 4;
         }
 
-        private void SettingListBitDataPool_SelectedIndexChanged(object sender, EventArgs e)
+        private void sendMsg(byte[] msg)
         {
-
+            try
+            {
+                serialPort.Write(msg, 0, msg.Length);
+                lastSentMsg = msg;
+            }
+            catch (Exception ex)
+            {
+                LogMessage(ex.Message);
+                return;
+            }
+        }
+        private void StartTransaction(byte[] msg)
+        {
+            if (pingWait)
+            {
+                timer.Interval = Convert.ToInt32(TimePing.Text) == 0 ? 1 : Convert.ToInt32(TimePing.Text);
+            } else
+            {
+                timer.Interval = numericUpDown3.Value == 0 ? 1 : (int)numericUpDown3.Value;
+                retransmisions = (int)numericUpDown2.Value;
+            }
+            timer.Start();
+            isTransaction = true;
+            sendMsg(msg);
+            return;
         }
 
-        private void SettingListStop_SelectedIndexChanged(object sender, EventArgs e)
+        private void EndTransaction()
         {
-
+            pingWait = false;
+            isTransaction = false;
+            retransmisions = 0;
+            buffer.Clear();
         }
 
-        private void panel2_Paint(object sender, PaintEventArgs e)
+        private bool IsPing(byte[] msg)
         {
-
+            if (msg == null)
+            {
+                return false;
+            }
+            if (msg.Length != 4)
+            {
+                return false;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (pingArr[i] != msg[i])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
-        private void SettingControlFlewComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        private bool IsPong(byte[] msg)
         {
-          
+            if (msg == null)
+            {
+                return false;
+            }
+            if (msg.Length != 4)
+            {
+                return false;
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                if (pongArr[i] != msg[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ping
+            pingWait = true;
+            StartTransaction(pingArr);
+        }
+        
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            if(TabControl.SelectedTab.Text == "RS232")
+            {
+                sendMsg(StringParser.StrToByteArray(textBox1.Text));
+                LogOutput.AppendText("Wysłano:/n" + textBox1.Text + "/n");
+                return;
+            }
+            if((numericUpDown7.Value == 0) && (numericUpDown6.Value == 2))
+            {
+                LogMessage("Rozkaz 2 nie może działać w trybie rozgłoszeniowym");
+                return;
+            }
+            string msgString = StringParser
+                .FormatMessageToModbus(textBox1.Text, (byte)numericUpDown6.Value, (byte)numericUpDown7.Value);
+            byte[] msg = StringParser.StrToByteArray(msgString);
+            if (MasterButtonRadio.Checked)
+            {
+                if(numericUpDown7.Value > 0)
+                {
+                    StartTransaction(msg);
+                    return;
+                }
+                sendMsg(msg);
+            }
         }
     }
 }
---
+++ RS232-GUI/RS232-GUI/StringParser.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RS232_GUI
+{
+    enum DataFormat { ASCII, HEX, DEC }
+
+    class StringParser
+    {
+        static Dictionary<char, byte> spCHArray = new Dictionary<char, byte>();
+
+        static StringParser()
+        {
+            spCHArray.Add('n', 10);
+            spCHArray.Add('r', 13);
+            spCHArray.Add('0', 9);
+            spCHArray.Add('\\', 92);
+        }
+
+        public static byte? SpecialCharacter(char sign)
+        {
+            if (spCHArray.ContainsKey(sign))
+                return spCHArray[sign];
+            else
+                return null;
+        }
+
+        public static string ByteToDisplay(byte[] byteArray, DataFormat p = DataFormat.ASCII)
+        {
+            StringBuilder text = new StringBuilder(byteArray.Length);
+
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                switch (p)
+                {
+                    case DataFormat.HEX:
+                        text.Append(" " + byteArray[i].ToString("X2") + " ");
+                        break;
+
+                    case DataFormat.DEC:
+                        text.Append("{d" + byteArray[i].ToString("D3") + "}");
+                        break;
+
+                    case DataFormat.ASCII:
+                        text.Append(Convert.ToChar(byteArray[i]));
+                        break;
+                }
+            }
+
+            return text.ToString();
+        }
+
+        public static byte[] StrToByteArray(string str)
+        {
+            List<byte> conBytes = new List<byte>();
+            int i;
+
+            for (i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '\\')
+                {
+                    if (i + 1 == str.Length)
+                        break;
+
+                    switch (str[i + 1])
+                    {
+                        case 'x':
+                            if (i + 3 == str.Length)
+                                break;
+
+                            conBytes.Add(Convert.ToByte(str.Substring(i + 2, 2), 16));
+                            i += 3;
+                            break;
+                        case 'd':
+                            if (i + 4 == str.Length)
+                                break;
+
+                            conBytes.Add(Convert.ToByte(str.Substring(i + 2, 3), 10));
+                            i += 4;
+                            break;
+
+                        default:
+                            byte? spec = SpecialCharacter(str[++i]);
+
+                            if (spec != null)
+                                conBytes.Add(spec.Value);
+
+                            break;
+                    }
+                }
+                else
+                    conBytes.Add(Convert.ToByte(str[i]));
+            }
+
+            if (i != str.Length)
+                throw new Exception("Nie można sparsować stringa.");
+
+            return conBytes.ToArray();
+        }
+
+        public static string FormatMessageToModbus(string str, byte instrNumber, byte address)
+        {
+            byte begin = (byte)58;
+
+            byte[] myByte = System.Text.ASCIIEncoding.Default.GetBytes(str);
+
+            List<byte> bytes = new List<byte>();
+            bytes.Add(address);
+            bytes.Add(instrNumber);
+            foreach (byte b in myByte)
+            {
+                bytes.Add(b);
+            }
+            byte lrc = bytes.Aggregate<byte, byte>(0, (x, y) => (byte)(x ^ y));
+            bytes.Add(lrc);
+            StringBuilder hex = new StringBuilder(bytes.Count * 2);
+            foreach (byte b in bytes)
+                hex.AppendFormat("{0:x2}", b);
+            string hexMessage = hex.ToString();
+            hexMessage = Convert.ToChar(begin) + hexMessage + Convert.ToChar((byte)13) + Convert.ToChar((byte)10);
+            return hexMessage;
+        }
+
+        public static bool CheckModbusMessage(byte[] frame, byte address)
+        {
+            try
+            {
+                if (frame.Length < 9)
+                {
+                    return false;
+                }
+                if (frame.Length % 2 == 0)
+                {
+                    return false;
+                }
+                if (frame[0] != 58)
+                {
+                    return false;
+                }
+                if ((ByteHexToInt(frame[1]) * 16 + ByteHexToInt(frame[2]) != address) && (ByteHexToInt(frame[1]) * 16 + ByteHexToInt(frame[2]) != 0))
+                {
+                    return false;
+                }
+                int instr = ByteHexToInt(frame[3]) * 16 + ByteHexToInt(frame[4]);
+                if ((instr == 2) && (ByteHexToInt(frame[1]) * 16 + ByteHexToInt(frame[2]) > 1))
+                {
+                    return false;
+                }
+                if ((instr < 1) || (instr > 2))
+                {
+                    return false;
+                }
+                if ((frame[frame.Length - 2] != 13) || (frame[frame.Length - 1] != 10))
+                {
+                    return false;
+                }
+                List<byte> message = new List<byte>();
+                for (int i = 1; i < frame.Length - 4; i += 2)
+                {
+                    message.Add((byte)(ByteHexToInt(frame[i]) * 16 + ByteHexToInt(frame[i + 1])));
+                }
+                byte lrc = (byte)(ByteHexToInt(frame[frame.Length - 4]) * 16 + ByteHexToInt(frame[frame.Length - 3]));
+                if ((message.Count == 2) && (lrc == 0))
+                {
+                    return true;
+                }
+                byte trueLrc = message.Aggregate<byte, byte>(0, (x, y) => (byte)(x ^ y));
+                if (lrc != trueLrc)
+                {
+                    return false;
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+        private static byte ByteHexToInt(byte hex)
+        {
+            char c = Convert.ToChar(hex);
+            string hexStr = c.ToString();
+            return (byte)Convert.ToInt32(hexStr, 16);
+        }
+
+        public static byte[] GetModbusMessage(byte[] msg)
+        {
+            if (msg.Length == 9)
+            {
+                return new byte[0];
+            }
+            List<byte> message = new List<byte>();
+            for (int i = 5; i < msg.Length - 4; i += 2)
+            {
+                message.Add((byte)(ByteHexToInt(msg[i]) * 16 + ByteHexToInt(msg[i + 1])));
+            }
+            return message.ToArray();
+        }
+    }
+}
